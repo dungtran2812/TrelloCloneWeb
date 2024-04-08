4cf1a1a519429f7afc1747f8b1de9da982c7095e
dungtran2812 <dunghoclaptrinh2812@gmail.com>
2024-04-08 20:44:18 +0700

    add_system_theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,47 @@
 import { useColorScheme } from '@mui/material/styles'
 import Button from '@mui/material/Button'
+import Box from '@mui/material/Box'
+import InputLabel from '@mui/material/InputLabel'
+import MenuItem from '@mui/material/MenuItem'
+import FormControl from '@mui/material/FormControl'
+import Select from '@mui/material/Select'
+import LightModeIcon from '@mui/icons-material/LightMode'
+import DarkModeIcon from '@mui/icons-material/DarkMode'
+import SettingsBrightnessIcon from '@mui/icons-material/SettingsBrightness'
+
+
+function SelectMode() {
+  const { mode, setMode } = useColorScheme()
+  const handleChange = (event) => {
+    const selectedMode = event.target.value
+    setMode(selectedMode)
+  }
+
+  return (
+    <Box sx={{ minWidth: 120 }}>
+      <FormControl fullWidth>
+        <InputLabel id="label-select-dark-light-mode">Mode</InputLabel>
+        <Select
+          labelId="label-select-dark-light-mode"
+          id="select-dark-light-mode"
+          value={mode}
+          label="Mode"
+          onChange={handleChange}
+        >
+          <MenuItem value="light">
+            <Box sx={{ display:'flex', alignItems:'center', gap:1 }}><LightModeIcon fontSize='small'/>light</Box>
+          </MenuItem>
+          <MenuItem value="dark">
+            <Box sx={{ display:'flex', alignItems:'center', gap:1 }}><DarkModeIcon fontSize='small'/>Dark</Box>
+          </MenuItem>
+          <MenuItem value="system">
+            <Box sx={{ display:'flex', alignItems:'center', gap:1 }}><SettingsBrightnessIcon fontSize='small'/>System</Box>
+          </MenuItem>
+        </Select>
+      </FormControl>
+    </Box>
+  )
+}
 
 function ModeToggle() {
   const { mode, setMode } = useColorScheme()
@@ -16,10 +58,10 @@ function ModeToggle() {
 
 function App() {
 
-
   return (
     <>
-      <ModeToggle/>
+      <SelectMode />
+      <ModeToggle />
     </>
   )
 }
