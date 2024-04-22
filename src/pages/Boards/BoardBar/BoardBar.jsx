fa74070ddf116af5ea10baf624f4799d855de149
dungtran2812 <dunghoclaptrinh2812@gmail.com>
2024-04-22 17:02:03 +0700

    complete drag and drop column

--- src/pages/Boards/BoardBar/BoardBar.jsx
+++ src/pages/Boards/BoardBar/BoardBar.jsx
@@ -106,37 +106,37 @@ function BoardBar({ board }) {
             <Tooltip title='dungtran'>
               <Avatar
                 alt="dungtran"
-                src="https://avatars.githubusercontent.com/u/155450761?v=4" 
+                src="https://scontent.fsgn4-1.fna.fbcdn.net/v/t39.30808-1/427882541_1377651606454468_2650009597030579197_n.jpg?stp=dst-jpg_p320x320&_nc_cat=105&ccb=1-7&_nc_sid=5f2048&_nc_ohc=_gq8FeNS8vQAb6PRVm7&_nc_ht=scontent.fsgn4-1.fna&oh=00_AfCRniZ2xODWmEmGcQCKmrNJQ44aziHezv_TEX6LkOAnnw&oe=662BF64D"
               />
             </Tooltip>
             <Tooltip title='dungtran'>
               <Avatar
                 alt="dungtran"
-                src="https://avatars.githubusercontent.com/u/155450761?v=4" 
+                src="https://scontent.fsgn13-2.fna.fbcdn.net/v/t39.30808-1/434655765_2186833181666902_8919608324179202828_n.jpg?stp=dst-jpg_p320x320&_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=LvwDeJzcUY8Ab5eIq0K&_nc_ht=scontent.fsgn13-2.fna&oh=00_AfDxszq97AcKnVsD-XJadfLNG1UT2QXg_NEkWuMDZnc3rA&oe=662C004B"
               />
             </Tooltip>
             <Tooltip title='dungtran'>
               <Avatar
                 alt="dungtran"
-                src="https://avatars.githubusercontent.com/u/155450761?v=4" 
+                src="https://scontent.fsgn13-2.fna.fbcdn.net/v/t39.30808-1/432783625_968395744916002_7562829046057230045_n.jpg?stp=dst-jpg_s320x320&_nc_cat=109&ccb=1-7&_nc_sid=5f2048&_nc_ohc=Z7910qWKu2MAb76I3z0&_nc_ht=scontent.fsgn13-2.fna&oh=00_AfD9TQB6rXEG4JNHdWMJhjltERE9IBiyqGAvpaEKGGKKOg&oe=662BFED7"
               />
             </Tooltip>
             <Tooltip title='dungtran'>
               <Avatar
                 alt="dungtran"
-                src="https://avatars.githubusercontent.com/u/155450761?v=4" 
+                src="https://scontent.fsgn3-1.fna.fbcdn.net/v/t39.30808-1/437784898_1261571224811485_3822865762562856088_n.jpg?stp=dst-jpg_p320x320&_nc_cat=107&ccb=1-7&_nc_sid=5f2048&_nc_ohc=yiwgqwajgKMAb7VApLN&_nc_ht=scontent.fsgn3-1.fna&oh=00_AfDXaWaLjrO7lUkCqoK6ZUhtcH1uyRbCfMgwvUlRGUpnSw&oe=662C0C21"
               />
             </Tooltip>
             <Tooltip title='dungtran'>
               <Avatar
                 alt="dungtran"
-                src="https://avatars.githubusercontent.com/u/155450761?v=4" 
+                src="https://scontent.fsgn4-1.fna.fbcdn.net/v/t39.30808-1/246142451_1088632288545462_1655627781083416421_n.jpg?stp=dst-jpg_p320x320&_nc_cat=105&ccb=1-7&_nc_sid=5f2048&_nc_ohc=jLoNgqsCo8kAb4vX4hC&_nc_ht=scontent.fsgn4-1.fna&oh=00_AfC50A9eF78NUOEX0FfDMhgloc-YnV7I9CqzHIniMuKyCw&oe=662BFC49"
               />
             </Tooltip>
             <Tooltip title='dungtran'>
               <Avatar
                 alt="dungtran"
-                src="https://avatars.githubusercontent.com/u/155450761?v=4" 
+                src="https://scontent.fsgn13-2.fna.fbcdn.net/v/t39.30808-1/364708829_1714521608971734_7437894059025187558_n.jpg?stp=dst-jpg_p320x320&_nc_cat=106&ccb=1-7&_nc_sid=5f2048&_nc_ohc=T2hx8oTMCrwAb5wptQT&_nc_ht=scontent.fsgn13-2.fna&oh=00_AfAX79SjYRS5B34hZQEqGNRylYOvsIVVIsjkIEjDcO_ZHw&oe=662C17FF"
               />
             </Tooltip>
             <Tooltip title='dungtran'>
--- src/pages/Boards/BoardContent/BoardContent.jsx
+++ src/pages/Boards/BoardContent/BoardContent.jsx
@@ -1,23 +1,56 @@
+import {
+  DndContext,
+  // PointerSensor,
+  MouseSensor,
+  TouchSensor,
+  useSensor,
+  useSensors
+} from '@dnd-kit/core'
 import Box from '@mui/material/Box'
+import { useEffect, useState } from 'react'
 import { mapOrder } from '~/utils/sort'
 import ListColumns from './ListColumns/ListColumns'
+import { arrayMove } from '@dnd-kit/sortable'
 
 function BoardContent({ board }) {
-  const orderedColumns = mapOrder(board?.columns, board?.columnOrderIds, '_id')
+  // pointer sensor phai ket hop css touchaction none ở phần tử kéo thả và còn bug
+  // const pointerSensor = useSensor(PointerSensor, { activationConstraint: { distance: 10 } })
+  const mouseSensor = useSensor(MouseSensor, { activationConstraint: { distance: 10 } })
+  const touchSensor = useSensor(TouchSensor, { activationConstraint: { delay: 250, tolerance: 500 } })
+  const mySensors = useSensors(mouseSensor, touchSensor)
+  const [orderedColumns, setOrderedColumns] = useState([])
+  const handleDragEnd = (event) => {
+    // console.log('handle drag end:', event)
+    const { active, over } = event
+    if (!over) return
+    if (active.id !== over.id) {
+      const oldIndex = orderedColumns.findIndex(c => c._id === active.id) //old position from active
+      const newIndex = orderedColumns.findIndex(c => c._id === over.id) //new position from over
+      const dndOrderedColumns = arrayMove(orderedColumns, oldIndex, newIndex)
+      // const dndOrderedColumnsIds = dndOrderedColumns.map(c => c._id)
+      // console.log(dndOrderedColumns)
+      // console.log(dndOrderedColumnsIds)
+      setOrderedColumns(dndOrderedColumns)
+    }
+  }
+  useEffect(() => {
+    setOrderedColumns(mapOrder(board?.columns, board?.columnOrderIds, '_id'))
+  }, [board])
   return (
     <>
-      <Box sx={{
-        backgroundColor: 'primary.main',
-        width: '100%',
-        height: (theme) => theme.trello.boardContentHeight,
-        bgcolor: (theme) => (
-          theme.palette.mode === 'dark' ? '#34495e' : '#1976d2'
-        ),
-        p: '10px 0'
-
-      }}>
-        <ListColumns columns={orderedColumns}/>
-      </Box>
+      <DndContext onDragEnd={handleDragEnd} sensors={mySensors}>
+        <Box sx={{
+          backgroundColor: 'primary.main',
+          width: '100%',
+          height: (theme) => theme.trello.boardContentHeight,
+          bgcolor: (theme) => (
+            theme.palette.mode === 'dark' ? '#34495e' : '#1976d2'
+          ),
+          p: '10px 0'
+        }}>
+          <ListColumns columns={orderedColumns} />
+        </Box>
+      </DndContext>
     </>
   )
 }
