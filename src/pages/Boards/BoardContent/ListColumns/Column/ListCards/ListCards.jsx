bbb5ea28cdb23e3a7b5434be8f188b50363b4c07
dungtran2812 <dunghoclaptrinh2812@gmail.com>
2024-04-24 19:31:20 +0700

    drag and drop for card && add placeholder for on drag event

--- src/pages/Boards/BoardContent/ListColumns/Column/ListCards/ListCards.jsx
+++ src/pages/Boards/BoardContent/ListColumns/Column/ListCards/ListCards.jsx
@@ -1,6 +1,6 @@
 import Box from '@mui/material/Box'
 import Card from './Card/Card'
-
+import { SortableContext, verticalListSortingStrategy } from '@dnd-kit/sortable'
 
 
 function ListCards({ cards }) {
@@ -8,30 +8,33 @@ function ListCards({ cards }) {
   return (
     <>
       {/* List card */}
-      <Box sx={{
-        p: '0 5px',
-        m: '0 5px',
-        display: 'flex',
-        flexDirection: 'column',
-        gap: 1,
-        overflowX: 'hidden',
-        overflowY: 'auto',
-        maxHeight: (theme) => `calc(
+      <SortableContext items={cards?.map(c => c._id)} strategy={verticalListSortingStrategy}>
+        <Box sx={{
+          p: '0 5px',
+          m: '0 5px',
+          display: 'flex',
+          flexDirection: 'column',
+          gap: 1,
+          overflowX: 'hidden',
+          overflowY: 'auto',
+          maxHeight: (theme) => `calc(
                 ${theme.trello.boardContentHeight} -
                 ${theme.spacing(5)} -
                 ${theme.trello.columnHeaderHeight} -
                 ${theme.trello.columnFooterHeight})`,
-        '&::-webkit-scrollbar-thumb': {
-          backgroundColor: '#ced0da',
-          borderRadius: '8px'
-        },
-        '&::-webkit-scrollbar-thumb:hover': {
-          backgroundColor: '#bfc2cf'
-        }
-      }}>
-        {cards?.map(card => <Card key={card._id} card={card}/>)}
-        <Card/>
-      </Box>
+          '&::-webkit-scrollbar-thumb': {
+            backgroundColor: '#ced0da',
+            borderRadius: '8px'
+          },
+          '&::-webkit-scrollbar-thumb:hover': {
+            backgroundColor: '#bfc2cf'
+          }
+        }}>
+          {cards?.map(card => <Card key={card._id} card={card} />)}
+
+        </Box>
+      </SortableContext>
+
     </>
   )
 }
